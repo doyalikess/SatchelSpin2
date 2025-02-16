7ad6c92f8d214fdd72e962dd5b04152469754515
doyalikess <zachsaliba123@gmail.com>
2025-02-16 16:26:11 +0100

    Delete discord_login.php

--- discord_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-// discord_login.php
-
-// Redirect user to Discord's OAuth page
-header('Location: https://discord.com/oauth2/authorize?client_id=1340701003523297320&response_type=code&redirect_uri=https%3A%2F%2Fdoyalikess.github.io%2FSatchelSpin2%2Fdiscord_callback.php&scope=openid+identify');
-exit;
-?>
